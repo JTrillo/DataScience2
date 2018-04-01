77dd68037ca8c2b6659d5c1885cdec7870ecbb97
yasos <aoujil.yasser@gmail.com>
2018-04-01 23:53:44 +0200

    Python file adapted to new dataset for mongodb

--- process_insert.py
+++ process_insert.py
@@ -26,9 +26,9 @@ def parse_complete_date(s):
 # This function receives a row from the dataframe
 def parse_location(s):
     lat = s["Y"]
-    lng = s["X"]
+    log = s["X"]
     location_dict = {
-        'coordinates': [lng, lat],
+        'coordinates': [log, lat],
         'type': 'point'
     }
     return location_dict
@@ -36,22 +36,22 @@ def parse_location(s):
 
 def import_content(file_path):
     client = MongoClient()
-    db = client['san_francisco_incidents']  # Creates the database for the San Francisco city incidents data
+    db = client['san_francisco_incidents1']  # Creates the database for the San Francisco city incidents data
     incid = db.incidents        # Creates the collection of the documents of that will represent the incidents
     print("Reading csv file...")
     df = pd.read_csv(file_path)  # Reads the csv file into python's dataframe type (in my case I named it incid.csv)
     # We start parsing the dataframe columns
-    df["X"] = df["X"].apply(parse_float)  # We make sure the the fields X and Y are of type float
+    df["X"] = df["X"].apply(parse_float)  # We make sure the fields X and Y are of type float
     df["Y"] = df["Y"].apply(parse_float)
+    print("Parsing location...")
+    df["Location"] = df.apply(parse_location, axis=1)  # Parse location into a well-formatted geojson object
     print("Parsing date...")
     df["Date"] = df.apply(parse_complete_date, axis=1)  # Axis=1 to interate over the rows applying parse_complete_date to each one
-    print("Parsing location...")
-    df["Location"] = df.apply(parse_location, axis=1) # Parse location into a well-formatted geojson file
-    print("Inserting data into monogodb, might take some minutes, please be patient...")
-    incid.insert_many(df.to_dict("records")) # Insert many python objects into the collection incidents
-    print("Done :D ")
+    print("Inserting data into monogodb. Might take some minutes, please be patient...")
+    incid.insert_many(df.to_dict("records"))  # Insert many python objects into the collection incidents
+    print("Done :D")
 
 
 if __name__ == "__main__":
-    file_path = "/Users/yasos/Downloads/incidents.csv"
+    file_path = "~/incidents.csv"
     import_content(file_path)
