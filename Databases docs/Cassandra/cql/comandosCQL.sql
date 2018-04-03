4d08cf1014ba7ab5c5f19e82e7fd00a64162e69b
Ivan <ivaann2706@gmail.com>
2018-04-03 15:07:33 +0200

    docu cassandra y consultas

---
+++ Databases docs/Cassandra/cql/comandosCQL.sql
@@ -0,0 +1,68 @@
+#tabla con todos los campos completo
+CREATE TABLE incidencias.completoPdId( 
+   IncidntNum text, 
+   Category text,
+   Descript text,
+   DayOfWeek text,
+   Date date,
+   Time time,
+   PdDistrict text,
+   Resolution text,
+   Address Text,
+   X text,
+   Y text,
+   Location text,
+   PdId text,
+   PRIMARY KEY(PdId)
+);
+
+COPY incidencias.completoPdId (IncidntNum,Category,Descript,DayOfWeek,Date,Time,PdDistrict,Resolution,Address,X,Y,Location,PdId) FROM 'C:/Users/Ivan/Desktop/doccassandra/CSVs/completo.csv' WITH HEADER = TRUE;
+select COUNT(*) from incidencias.completoPdId;
+
+
+#ver actividad criminal por zonas de la ciudad
+CREATE TABLE incidencias.incidenciasbyzona( 
+   Category text,
+   Descript text,
+   Date date,
+   Time time,
+   PdDistrict text,
+   Resolution text,
+   Location text,
+   PdId text,
+   PRIMARY KEY(PdDistrict,PdId)
+)WITH CLUSTERING ORDER BY (PdId ASC);
+
+COPY incidencias.incidenciasbyzona (Category,Descript,Date,Time,PdDistrict,Resolution,Location,PdId) FROM 'C:/Users/Ivan/Desktop/doccassandra/CSVs/incidenciasbyzona.csv' WITH HEADER = TRUE;
+SELECT COUNT(*) FROM incidencias.incidenciasbyzona WHERE pddistrict='NORTHERN';
+SELECT * FROM incidencias.incidenciasbyzona WHERE pddistrict='NORTHERN';
+
+#ver las categorias de incidentes distintas que hay
+CREATE TABLE incidencias.categorias( 
+   Category text,
+   PRIMARY KEY(Category)
+);
+
+COPY incidencias.categorias (Category) FROM 'C:/Users/Ivan/Desktop/doccassandra/CSVs/categorias.csv' WITH HEADER = TRUE;
+SELECT COUNT(*) FROM incidencias.categorias;
+SELECT * FROM incidencias.categorias;
+
+
+#numero de incidentes por categoria y distrito
+CREATE TABLE incidencias.incidenciasbycategoriazona( 
+   Category text,
+   Descript text,
+   Date date,
+   Time time,
+   PdDistrict text,
+   Resolution text,
+   Location text,
+   PdId text,
+   PRIMARY KEY((Category,PdDistrict),PdId)
+)WITH CLUSTERING ORDER BY (PdId ASC);
+
+COPY incidencias.incidenciasbycategoriazona (Category,Descript,Date,Time,PdDistrict,Resolution,Location,PdId) FROM 'C:/Users/Ivan/Desktop/doccassandra/CSVs/incidenciasbycategoriazona.csv' WITH HEADER = TRUE;
+SELECT COUNT(*) FROM incidencias.incidenciasbycategoriazona WHERE  Category='SUICIDE' AND pddistrict='BAYVIEW';
+SELECT * FROM incidencias.incidenciasbycategoriazona WHERE Category='SUICIDE' AND pddistrict='BAYVIEW';
+
+
